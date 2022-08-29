7059858839cb2b391487196fcc628aa06c71b797
Ibrahim <ibrahimghali211@gmail.com>
2022-08-29 13:47:27 +0300

    Add Mongodb connection string

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dbURI = process.env.MONGODB_CONNECTION_STRING;
 
 app.set('view engine', 'ejs');
 
-app.listen(
-  PORT,
-  console.log(`Listening for requests on port http://localhost:${PORT}`)
-);
+mongoose
+  .connect(dbURI)
+  .then(result => app.listen(PORT))
+  .catch(err => console.log(err));
 
 app.use(express.urlencoded({ extended: true }));
 
