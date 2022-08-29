e87434493f87a42877aad29fae25fd024ee7b17a
Ibrahim <ibrahimghali211@gmail.com>
2022-08-29 12:49:10 +0300

    Add index.ejs

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.listen(
   PORT,
   console.log(`Listening for requests on port http://localhost:${PORT}`)
 );
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
